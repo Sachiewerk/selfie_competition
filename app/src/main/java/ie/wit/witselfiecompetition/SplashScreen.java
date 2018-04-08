4db4584ac22292564749b7d06168179dd0ed6986
Yahya Almardeny <almardeny@gmail.com>
2018-04-08 12:54:09 +0100

    changed EditText hint colour

--- app/src/main/java/ie/wit/witselfiecompetition/SplashScreen.java
+++ app/src/main/java/ie/wit/witselfiecompetition/SplashScreen.java
@@ -5,7 +5,6 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import java.util.concurrent.Callable;
 
@@ -54,8 +53,7 @@ public class SplashScreen extends AppCompatActivity {
                         };
 
 
-                        //App.copyUserInfoFromDatabaseToSharedPref(SplashScreen.this, postSuccessfulCopy);
-                        App.copyUserInfoFromDatabaseToSharedPrefAndRedirect(SplashScreen.this, Main.class, ProfileSetup.class);
+                        App.copyUserInfoFromDatabaseToSharedPref(SplashScreen.this, postSuccessfulCopy);
                     }
                     else{
                         App.redirect(SplashScreen.this, Login.class, false);
